162c61fd1d5e0a01e13a2226195cbfd87583a4b4
Saurabh Kumar Mishra <135007222+SaurabhKmMishra@users.noreply.github.com>
2025-08-17 13:47:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
 const hamburgerBtn = document.querySelector('.hamburger');
+const main = document.querySelector('main');
 const menuLinks = document.querySelector('#Menulinks');
 const cancelIcon = document.querySelector('.cancelIcon')
 
 hamburgerBtn.addEventListener('click', ()=>{
     menuLinks.classList.add('activeMenues');
-    // menuLinks.classList.remove('anchorTags');//
     cancelIcon.classList.add('show');
 
     hamburgerBtn.classList.add('hide');
     menuLinks.classList.remove('move');
 
 
-
 });
 
-cancelIcon.addEventListener('click', () => {
+cancelIcon.addEventListener('click', removeAnchorLinks);
+    
+main.addEventListener('click', removeAnchorLinks);
+
+function removeAnchorLinks(){
     cancelIcon.classList.remove('show');
     hamburgerBtn.classList.remove('hide');
     menuLinks.classList.add('move');
-    menuLinks.classList.remove('activeMenues');
-    menuLinks.classList.add('anchorTags'); //
-});
+}
